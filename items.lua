72c9ba296312f8749968e3afecdf4b62cf3d492d
madeinJPN <73656007+madeinJPN@users.noreply.github.com>
2025-07-24 02:08:41 -0400

    add ox_inventory items

---
+++ items.lua
@@ -0,0 +1,32 @@
+return {
+    barrel = {
+        label = 'Barrel',
+        weight = 10000,
+        stack = true,
+        description = 'Contenedor para almacenar crudo'
+    },
+    crude_oil = {
+        label = 'Crude Oil',
+        weight = 1000,
+        stack = true,
+        description = 'Petr\xc3\xb3leo sin refinar'
+    },
+    wrench = {
+        label = 'Wrench',
+        weight = 500,
+        stack = false,
+        description = 'Herramienta de mantenimiento'
+    },
+    oil_filter = {
+        label = 'Oil Filter',
+        weight = 250,
+        stack = true,
+        description = 'Filtro para trabajos de mantenimiento'
+    },
+    advanced_oil_kit = {
+        label = 'Advanced Oil Kit',
+        weight = 750,
+        stack = true,
+        description = 'Kit de mantenimiento avanzado'
+    }
+}
